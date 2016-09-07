4ff27cb01e1c89a0649dec2917a92883703e5a25
DDRDmakar <makarevich.98@mail.ru>
2016-09-07 13:23:11 +0300

    Update IfElse.kt

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -1,31 +1,31 @@
 @file:Suppress("UNUSED_PARAMETER")
-package lesson2.task1
+        package lesson2.task1
 
-import lesson1.task1.discriminant
+        import lesson1.task1.discriminant
 
 /**
  * Пример
  *
  * Найти наименьший корень биквадратного уравнения ax^4 + bx^2 + c = 0
  */
-fun minBiRoot(a: Double, b: Double, c: Double): Double {
-    // 1: в главной ветке if выполняется НЕСКОЛЬКО операторов
-    if (a == 0.0) {
+        fun minBiRoot(a: Double, b: Double, c: Double): Double {
+        // 1: в главной ветке if выполняется НЕСКОЛЬКО операторов
+        if (a == 0.0) {
         if (b == 0.0) return Double.NaN // ... и ничего больше не делать
         val bc = -c / b
         if (bc < 0.0) return Double.NaN // ... и ничего больше не делать
         return -Math.sqrt(bc)
         // Дальше функция при a == 0.0 не идёт
-    }
-    val d = discriminant(a, b, c)   // 2
-    if (d < 0.0) return Double.NaN  // 3
-    // 4
-    val y1 = (-b + Math.sqrt(d)) / (2 * a)
-    val y2 = (-b - Math.sqrt(d)) / (2 * a)
-    val y3 = Math.max(y1, y2)       // 5
-    if (y3 < 0.0) return Double.NaN // 6
-    return -Math.sqrt(y3)           // 7
-}
+        }
+        val d = discriminant(a, b, c)   // 2
+        if (d < 0.0) return Double.NaN  // 3
+        // 4
+        val y1 = (-b + Math.sqrt(d)) / (2 * a)
+        val y2 = (-b - Math.sqrt(d)) / (2 * a)
+        val y3 = Math.max(y1, y2)       // 5
+        if (y3 < 0.0) return Double.NaN // 6
+        return -Math.sqrt(y3)           // 7
+        }
 
 /**
  * Простая
@@ -33,7 +33,21 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+        fun ageDescription(age: Int): String {
+
+        String answer
+        Int last_digit = age % 10
+
+        if ( last_digit == 1 ) answer = "$age год"
+        if (
+        last_digit == 2 ||
+        last_digit == 3 ||
+        last_digit == 4
+        ) answer = "$age года"
+        if ( last_digit > 4 || ( age >= 10 && age <= 21 ) ) answer = "$age лет"
+        
+        return answer
+        }
 
 /**
  * Простая
@@ -42,9 +56,27 @@ fun ageDescription(age: Int): String = TODO()
  * и t3 часов — со скоростью v3 км/час.
  * Определить, за какое время он одолел первую половину пути?
  */
-fun timeForHalfWay(t1: Double, v1: Double,
-                   t2: Double, v2: Double,
-                   t3: Double, v3: Double): Double = TODO()
+        fun timeForHalfWay(t1: Double, v1: Double, t2: Double, v2: Double, t3: Double, v3: Double): Double {
+        
+        Double answer, s1, s2, s3, half
+        
+        s1 = t1 * v1
+        s2 = t2 * v2
+        s3 = t3 * v3
+        
+        half = ( s1 + s2 + s3 ) / 2
+        
+        // V T
+        
+        if ( half <= s1 ) answer = half / v1
+        
+        else if ( half > s1 && half <= s1 + s2 ) answer = t1 + ( ( half - s1 ) / v2 )
+        
+        else answer = t1 + t2 + ( half - s1 - s2 ) / v3
+        
+        return answer
+        
+        }
 
 /**
  * Простая

b264ea6e030258b3c8771632b36b7189a3573f5b
VirusVoid <aleksa.sumenko@yandex.ru>
2016-09-07 14:40:18 +0400

    some tasks solved

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -51,7 +51,10 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
+    val r = seconds+(minutes*60)+(hours*60*60)
+    return r
+}
 
 /**
  * Тривиальная
@@ -60,15 +63,21 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double{
+    val q = ((vershoks*4.445)+(arshins*16*4.445)+(sagenes*48*4.445))/100
+    return q
+}
 
 /**
  * Тривиальная
  *
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
- * Вывести значение того же угла в радианах (например, 0.63256).
+ * Вывести значение того же угла в радианах (например, 0.63256).     рад=180/пи, 1град=60 мин=3600сек, град*пи/180
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double{
+    val y = (sec/3600.0+min/60.0+grad)*PI/180
+    return y
+}
 
 /**
  * Тривиальная

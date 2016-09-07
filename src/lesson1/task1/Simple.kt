c0ce1744771efcb2ca59014afd288f473975a155
Игорь <ardar.igor@mail.ru>
2016-09-07 14:15:21 +0400

    I did

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -110,4 +110,6 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  *Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int {
+    return ((number % 10) * 100) + ((number / 100)) + (number - ((number / 100)*100) - (number % 10))
+}

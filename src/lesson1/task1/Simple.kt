96d262902f5ca723a551e39833f0bfdd1b29e57b
xslavakarachev <mr.1990slava@mail.ru>
2016-09-08 20:05:31 +0300

    zadacha1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -88,7 +88,11 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double
+{
+    val allLong =  sqrt(sqr(x2-x1)+sqr(y2-y1))
+    return allLong
+}
 
 /**
  * Простая

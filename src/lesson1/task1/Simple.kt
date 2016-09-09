040b83c18ff836375243d3d2a5b2f25037c33dad
Denis Grafov <grafov.denis@gmail.com>
2016-09-09 19:44:21 +0300

    Доделал остальные задачи (сорри за флуд)

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -114,7 +114,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
 fun accountInThreeYears(initial: Int, percent: Int): Double {
-    return (((initial*(1+percent/100.0))*(1+percent/100.0))*(1+percent/100.0))
+    return (((initial*(1 + percent/100.0))*(1 + percent/100.0))*(1 + percent/100.0))
 }
 
 /**

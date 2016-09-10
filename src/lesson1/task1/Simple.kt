8ebc5ea0b75b60b9781be8e4522bea113edb8973
JulyMak <makarova.y672@gmail.com>
2016-09-10 17:48:38 +0300

    Задача5

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -99,7 +99,10 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int {
+    val digit = (number / 100) % 10
+    return  digit
+}
 
 /**
  * Простая

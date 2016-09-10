caa6bc5c4aba4a6c4316d034ab3a0ace08363d2b
JulyMak <makarova.y672@gmail.com>
2016-09-10 17:55:14 +0300

    Задача5

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -111,7 +111,10 @@ fun thirdDigit(number: Int): Int {
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
+    val allTime = (hoursArrive * 60 + minutesArrive) - (hoursDepart * 60 + minutesDepart)
+    return allTime
+}
 
 /**
  * Простая

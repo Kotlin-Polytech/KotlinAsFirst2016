2b345c309f50fb2bd6ab201019d3018fbc8c19c5
AnastasiyaTarasova <tanastasiiaa@gmail.com>
2016-09-12 17:19:15 +0400

    Выполнила все задания из первого урока.

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -76,7 +76,7 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double {  return (( grad +(( m
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double { return sqrt(sqr(x2 - x1) + sqr(y2 - y1)) }
 
 /**
  * Простая
@@ -84,7 +84,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int { return (number/100)%10}
 
 /**
  * Простая
@@ -93,7 +93,7 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int { return ((hoursArrive * 60 + minutesArrive) - (hoursDepart * 60 + minutesDepart)) }
 
 /**
  * Простая
@@ -102,7 +102,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double { return ((initial+initial*0.01*percent) + (initial+initial*0.01*percent)*0.01*percent) + ((initial+initial*0.01*percent) + (initial+initial*0.01*percent)*0.01*percent)*0.01*percent}
 
 /**
  * Простая
@@ -110,4 +110,7 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  *Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int {val x1 = number / 100
+                                    val x2 = (number / 10) % 10
+                                    val x3 = number % 10
+                                    return x3*100+x2*10+x1 }

1d7d7b86c1048e1cb83dcd85f78b3f88596c2f78
Женя <kj2806@mail.ru>
2016-09-13 15:57:18 +0300

    Сделаны все задачи из первого урока и задача из второго.

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -33,7 +33,11 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
+    if ((age in 5..19) || age%10==0 || age%10>4)
+    return age.toString() + " лет"
+    else if (age%10 == 1) return age.toString()+" год"
+    else return age.toString() + " года"}
 
 /**
  * Простая
@@ -45,7 +49,6 @@ fun ageDescription(age: Int): String = TODO()
 fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
                    t3: Double, v3: Double): Double = TODO()
-
 /**
  * Простая
  *

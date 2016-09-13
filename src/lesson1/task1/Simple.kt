c4f3d0dc41ebde4a7c7b6d354f5380749cdc2466
makosenkov <makosenkov@gmail.com>
2016-09-13 21:42:02 +0300

    kotlin

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -68,7 +68,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (vershok
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (sec + min * 60.0 + grad * 60.0 * 60.0)/3600.0 / (180/ PI)
 
 /**
  * Тривиальная
@@ -93,7 +93,7 @@ fun thirdDigit(number: Int): Int = (number/100)%10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = (minutesArrive + hoursArrive * 60) - (minutesDepart + hoursDepart * 60)
 
 /**
  * Простая
@@ -102,7 +102,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double = initial * (1.0 + percent / 100.0) * (1.0 + percent / 100.0) * (1.0 + percent / 100.0)
 
 /**
  * Простая

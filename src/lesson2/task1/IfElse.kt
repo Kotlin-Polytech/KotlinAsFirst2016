01fd599678b26f05aa401ad21175377dfda1d876
makosenkov <makosenkov@gmail.com>
2016-09-14 13:30:13 +0300

    kotlin

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -33,8 +33,14 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
+   if ((age in 5..20) || (age in 105..120)) return "$age лет"
+        if ((age%10) in 2..4) return "$age года"
+        if ((age%10)==1) return "$age год"
+        return "$age лет"
 
+
+}
 /**
  * Простая
  *

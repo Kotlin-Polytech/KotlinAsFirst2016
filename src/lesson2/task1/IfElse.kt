8ebd7978531b591b2fbb75cba725fbf2c6cb8881
Женя <kj2806@mail.ru>
2016-09-15 12:16:38 +0300

    Сделаны все задачи из второго урока, кроме двух последних.

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -48,7 +48,17 @@ fun ageDescription(age: Int): String {
  */
 fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
-                   t3: Double, v3: Double): Double = TODO()
+                   t3: Double, v3: Double): Double {
+    val s1 = t1 * v1
+    val s2 = t2 * v2
+    val s3 = t3 * v3
+    val put: Double = ((s1 + s2 + s3) / 2.0)
+    val time: Double
+    if (put <= s1) {time = (put / v1)}
+    else
+        if ((put > s1) && (put <= s1 + s2)) {time = (t1 + (put - s1) / v2)}
+        else {time = (t1+t2+(put - s1 - s2) / v3)}
+    return(time)}
 /**
  * Простая
  *
@@ -59,7 +69,12 @@ fun timeForHalfWay(t1: Double, v1: Double,
  */
 fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
-                       rookX2: Int, rookY2: Int): Int = TODO()
+                       rookX2: Int, rookY2: Int): Int {
+if (((kingX==rookX1)||(kingY==rookY1))&&(kingX==rookX2||(kingY==rookY2))) return(3)
+else if (((kingX!=rookX1)&& (kingY!=rookY1)) && ((kingX==rookX2) || (kingY==rookY2))) return(2)
+    else if (((kingX==rookX1)||(kingY==rookY1)) && ((kingX!=rookX2) && (kingY!=rookY2))) return(1)
+else return(0)
+}
 
 /**
  * Простая
@@ -72,24 +87,27 @@ fun whichRookThreatens(kingX: Int, kingY: Int,
  */
 fun rookOrBishopThreatens(kingX: Int, kingY: Int,
                           rookX: Int, rookY: Int,
-                          bishopX: Int, bishopY: Int): Int = TODO()
+                          bishopX: Int, bishopY: Int): Int  {
+    if (((kingX == rookX) || (kingY == rookY)) && ((kingX - bishopX) == (kingY - bishopY))) return(3) else
+        if (((kingX != rookX) && (kingY != rookY)) && ((Math.abs(kingX - bishopX)) == Math.abs(kingY - bishopY))) return(2) else
+            if (((kingX == rookX) || (kingY == rookY)) && ((kingX - bishopX) != (kingY - bishopY))) return(1) else return(0)
+}
+    /**
+     * Простая
+     *
+     * Треугольник задан длинами своих сторон a, b, c.
+     * Проверить, является ли данный треугольник остроугольным (вернуть 0),
+     * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
+     * Если такой треугольник не существует, вернуть -1.
+     */
+    fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
 
-/**
- * Простая
- *
- * Треугольник задан длинами своих сторон a, b, c.
- * Проверить, является ли данный треугольник остроугольным (вернуть 0),
- * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
- * Если такой треугольник не существует, вернуть -1.
- */
-fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
-
-/**
- * Средняя
- *
- * Даны четыре точки на одной прямой: A, B, C и D.
- * Координаты точек a, b, c, d соответственно, b >= a, d >= c.
- * Найти длину пересечения отрезков AB и CD.
- * Если пересечения нет, вернуть -1.
- */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+    /**
+     * Средняя
+     *
+     * Даны четыре точки на одной прямой: A, B, C и D.
+     * Координаты точек a, b, c, d соответственно, b >= a, d >= c.
+     * Найти длину пересечения отрезков AB и CD.
+     * Если пересечения нет, вернуть -1.
+     */
+    fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()

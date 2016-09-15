be0d83bee84fe37b697cd2ca6d2d58d6ec1277ca
Sidorina_Test1 <elena_petrova98@mail.ru>
2016-09-15 12:18:58 +0300

    Весь решенный 1 тест.

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -41,8 +41,44 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
  */
 fun main(args: Array<String>) {
     // Решаем x^2 - 3*x + 2 = 0
-    val x1x2 = quadraticRootProduct(1.0, -3.0, 2.0)
-    println("Root product: $x1x2")
+    // val x1x2 = quadraticRootProduct(1.0, -3.0, 2.0)
+    // println("Root product: $x1x2")
+    println("fun seconds")
+    println("30035, seconds(8, 20, 35) - " + seconds(8, 20, 35))
+    println("86400, seconds(24, 0, 0) - " + seconds(24, 0, 0))
+    println("13, seconds(0, 0, 13) - " + seconds(0, 0, 13))
+    println("========================")
+    println("fun lengthInMeters")
+    println("18.98, lengthInMeters(8, 2, 11) - " + lengthInMeters(8, 2, 11))
+    println("2.13, lengthInMeters(1, 0, 0) - " + lengthInMeters(1, 0, 0))
+    println("========================")
+    println("fun angleInRadian")
+    println("0.63256, angleInRadian(36, 14, 35) - " + angleInRadian(36, 14, 35))
+    println("Math.PI / 2.0, angleInRadian(90, 0, 0) - " + angleInRadian(90, 0, 0))
+    println("========================")
+    println("fun trackLength")
+    println("5.0, trackLength(3.0, 0.0, 0.0, 4.0) - " + trackLength(3.0, 0.0, 0.0, 4.0))
+    println("1.0, trackLength(0.0, 1.0, -1.0, 1.0) - " + trackLength(0.0, 1.0, -1.0, 1.0))
+    println("1.41, trackLength(1.0, 1.0, 2.0, 2.0) - " + trackLength(1.0, 1.0, 2.0, 2.0))
+    println("========================")
+    println("fun thirdDigit")
+    println("8, thirdDigit(3801) - " + thirdDigit(3801))
+    println("1, thirdDigit(100) - " + thirdDigit(100))
+    println("0, thirdDigit(1000) - " + thirdDigit(1000))
+    println("========================")
+    println("fun travelMinutes")
+    println("216, travelMinutes(9, 25, 13, 1) - " + travelMinutes(9, 25, 13, 1))
+    println("1, travelMinutes(21, 59, 22, 0) - " + travelMinutes(21, 59, 22, 0))
+    println("========================")
+    println("fun accountInThreeYears")
+    println("133.1, accountInThreeYears(100, 10) - " + accountInThreeYears(100, 10))
+    println("1.0, accountInThreeYears(1, 0) - " + accountInThreeYears(1, 0))
+    println("104.0, accountInThreeYears(13, 100) - " + accountInThreeYears(13, 100))
+    println("========================")
+    println("fun numberRevert")
+    println("874, numberRevert(478) - " + numberRevert(478))
+    println("201, numberRevert(102) - " + numberRevert(102))
+    println("_______________________________")
 }
 
 /**
@@ -51,7 +87,7 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = seconds + minutes*60 + hours*3600
 
 /**
  * Тривиальная
@@ -60,7 +96,7 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (vershoks*4.445 + arshins*4.445*16 + sagenes*48*4.445)/100
 
 /**
  * Тривиальная
@@ -68,7 +104,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (Math.PI / 180)*(grad + min/60 + sec/3600)
 
 /**
  * Тривиальная
@@ -76,7 +112,7 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x2-x1)+sqr(y2-y1))
 
 /**
  * Простая
@@ -84,7 +120,9 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int = number/100%10
+
+
 
 /**
  * Простая
@@ -93,7 +131,8 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int):
+        Int = (minutesArrive + 60*hoursArrive) - (minutesDepart + 60*hoursDepart)
 
 /**
  * Простая
@@ -102,7 +141,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int):
+        Double = initial*(1+percent/100.0)*(1+percent/100.0)*(1+percent/100.0)
 
 /**
  * Простая
@@ -110,4 +150,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  *Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int = number%10*100 + number/10%10*10 + number/100%10

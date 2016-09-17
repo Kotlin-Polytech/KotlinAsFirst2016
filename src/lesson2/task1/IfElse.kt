ecaba6070e2516e273828fe122fd1ebdcb7ad744
Stupidman00 <bva1138@yandex.ru>
2016-09-17 20:00:22 +0300

    lesson2 task1

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -2,6 +2,8 @@
 package lesson2.task1
 
 import lesson1.task1.discriminant
+import java.lang.Math.*
+import java.util.*
 
 /**
  * Пример
@@ -14,17 +16,17 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
         if (b == 0.0) return Double.NaN // ... и ничего больше не делать
         val bc = -c / b
         if (bc < 0.0) return Double.NaN // ... и ничего больше не делать
-        return -Math.sqrt(bc)
+        return -sqrt(bc)
         // Дальше функция при a == 0.0 не идёт
     }
     val d = discriminant(a, b, c)   // 2
     if (d < 0.0) return Double.NaN  // 3
     // 4
-    val y1 = (-b + Math.sqrt(d)) / (2 * a)
-    val y2 = (-b - Math.sqrt(d)) / (2 * a)
-    val y3 = Math.max(y1, y2)       // 5
+    val y1 = (-b + sqrt(d)) / (2 * a)
+    val y2 = (-b - sqrt(d)) / (2 * a)
+    val y3 = max(y1, y2)       // 5
     if (y3 < 0.0) return Double.NaN // 6
-    return -Math.sqrt(y3)           // 7
+    return -sqrt(y3)           // 7
 }
 
 /**
@@ -54,7 +56,10 @@ fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
                    t3: Double, v3: Double): Double
 {
-    TODO()
+    val s: Double = v1*t1+v2*t2+v3*t3
+    if(v1*t1>=s/2) return (s/2)/(v1)
+    else if (v2*t2>=(s/2)-v1*t1) return ((s/2)-v1*t1)/v2+t1
+    else return ((s/2)-v1*t1-v2*t2)/v3+t1+t2
 }
 
 /**
@@ -65,9 +70,28 @@ fun timeForHalfWay(t1: Double, v1: Double,
  * Вернуть 0, если угрозы нет, 1, если угроза только от первой ладьи, 2, если только от второй ладьи,
  * и 3, если угроза от обеих ладей.
  */
+fun rookIsAttack(rookX: Int, rookY: Int,
+                 figureX: Int, figureY: Int): Boolean {
+    return  if ((rookX == figureX) || (rookY == figureY)) true
+            else false
+}
 fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
-                       rookX2: Int, rookY2: Int): Int = TODO()
+                       rookX2: Int, rookY2: Int): Int
+{
+    return when{
+        (!rookIsAttack(rookX1, rookY1, kingX, kingY))&&
+                (!rookIsAttack(rookX2, rookY2, kingX, kingY)) -> 0
+        (rookIsAttack(rookX1, rookY1, kingX, kingY))&&
+                (!rookIsAttack(rookX2, rookY2, kingX, kingY)) -> 1
+        (!rookIsAttack(rookX1, rookY1, kingX, kingY))&&
+                (rookIsAttack(rookX2, rookY2, kingX, kingY))  -> 2
+        (rookIsAttack(rookX1, rookY1, kingX, kingY))&&
+                (rookIsAttack(rookX2, rookY2, kingX, kingY))  -> 3
+        else                                                  -> -1
+    }
+
+}
 
 /**
  * Простая
@@ -78,9 +102,24 @@ fun whichRookThreatens(kingX: Int, kingY: Int,
  * Вернуть 0, если угрозы нет, 1, если угроза только от ладьи, 2, если только от слона,
  * и 3, если угроза есть и от ладьи и от слона.
  */
+fun bishopIsAttack(bishopX: Int, bishopY: Int,
+                   figureX: Int, figureY: Int): Boolean
+{
+    return if(abs(figureX-bishopX) == abs(figureY-bishopY)) true
+           else false
+}
 fun rookOrBishopThreatens(kingX: Int, kingY: Int,
                           rookX: Int, rookY: Int,
-                          bishopX: Int, bishopY: Int): Int = TODO()
+                          bishopX: Int, bishopY: Int): Int
+{
+    return if((!rookIsAttack(rookX,rookY,kingX,kingY))&&
+            (!bishopIsAttack(bishopX,bishopY,kingX,kingY))) 0
+           else if((rookIsAttack(rookX,rookY,kingX,kingY))&&
+            (!bishopIsAttack(bishopX,bishopY,kingX,kingY))) 1
+           else if((!rookIsAttack(rookX,rookY,kingX,kingY))&&
+            (bishopIsAttack(bishopX,bishopY,kingX,kingY))) 2
+           else 3
+}
 
 /**
  * Простая
@@ -90,7 +129,16 @@ fun rookOrBishopThreatens(kingX: Int, kingY: Int,
  * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
  * Если такой треугольник не существует, вернуть -1.
  */
-fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
+fun triangleKind(a: Double, b: Double, c: Double): Int
+{
+    return when{
+        ((a+b<=c)||(a+c<=b)||(b+c<=a))                      -> -1
+        ((a*a+b*b<c*c)||(a*a+c*c<b*b)||(b*b+c*c<a*a))       -> 2
+        ((a*a+b*b==c*c)||(a*a+c*c==b*b)||(b*b+c*c==a*a))    -> 1
+        ((a*a+b*b>c*c)||(a*a+c*c>b*b)||(b*b+c*c>a*a))       -> 0
+        else                                                -> -1
+    }
+}
 
 /**
  * Средняя
@@ -100,4 +148,15 @@ fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
  * Найти длину пересечения отрезков AB и CD.
  * Если пересечения нет, вернуть -1.
  */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int
+{
+    val AB: Int = abs(a-b)
+    val AC: Int = abs(a-c)
+    val AD: Int = abs(a-d)
+    val BC: Int = abs(b-c)
+    val BD: Int = abs(b-d)
+    val CD: Int = abs(c-d)
+    val maxSegment: Int = max(max(max(AB,AC),max(AD,BC)),max(BD,CD))
+    val intersection = (AB+CD)-maxSegment
+    return if (intersection>=0) intersection else -1
+}

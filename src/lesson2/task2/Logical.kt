88a31e72a1a2eca142b800a58ac544e97a01e299
SidorenkoNikita <bioshock@list.ru>
2016-09-19 15:31:51 +0300

    Lesson2 task 2 queen threatens

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -31,7 +31,7 @@ fun isNumberHappy(number: Int): Boolean {
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  */
 fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
-    if ((x1 == x2) || (y1 == y2) || (x1+y1 == x2 + y2)) return true
+    if ((x1 == x2) || (y1 == y2) || (x1+y1 == x2 + y2)||(x1-y1==x2-y2)) return true
     else return false
 }
 

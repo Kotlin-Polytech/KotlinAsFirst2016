ba962100703b94c9eed047ada462e797dd32f71e
IgorLo <9440533@gmail.com>
2016-09-20 23:40:51 +0300

    TASK 2 ALL DONE

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -17,7 +17,10 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    if (number/1000 + ((number/100)%10) == (number%10 + (number/100)%10)) return true
+    return false
+}
 
 /**
  * Простая
@@ -25,7 +28,14 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * На шахматной доске стоят два ферзя (ферзь бьет по вертикали, горизонтали и диагоналям).
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    when {
+    x1 == x2 -> return true
+    y1 == y2 -> return true
+    Math.abs(x1-x2) == Math.abs(y1-y2) -> return true
+    }
+    return false
+}
 
 /**
  * Средняя
@@ -35,7 +45,10 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean {
+    if (((Math.sqrt((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2))) + r1) <= r2) return true
+    return false
+}
 
 /**
  * Средняя
@@ -46,4 +59,9 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    if (s>=a && (r>=b || r>=c)) return true
+    if (s>=b && (r>=a || r>=c)) return true
+    if (s>=c && (r>=b || r>=a)) return true
+    return false
+}

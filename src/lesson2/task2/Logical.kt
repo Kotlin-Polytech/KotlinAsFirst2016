f0f5e5f177c16aff55a8c6b0e20f3397f83d756b
тресков <treskovnikitaa@gmail.com>
2016-09-21 13:32:45 +0300

    lesson 2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -17,7 +17,14 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val a = (number / 1000)
+    val b = ((number / 100) % 10)
+    val c = ((number / 10) % 10)
+    val d = (number % 10)
+       return if ((a + b) == (c + d)) true
+    else false
+}
 
 /**
  * Простая

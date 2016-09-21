a9c7a1bd3c2822caec19f92b7da37f3128ba0ebe
TTo3uTuBHbiu <ilya_ivanov_9797@mail.ru>
2016-09-21 23:14:37 +0300

    Ilya Ivanov,lesson2/task2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -17,7 +17,12 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int):Boolean  {
+    val firstSum=((number/1000)+((number%1000)/100))
+    val secondSum=((((number%1000)%100)%10)+(((number%1000)%100)/10))
+    return if (firstSum==secondSum)true
+    else false
+}
 
 /**
  * Простая
@@ -25,7 +30,10 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * На шахматной доске стоят два ферзя (ферзь бьет по вертикали, горизонтали и диагоналям).
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    return if((x1==x2)&&(y1==y2)&&(x2-x1!=y2-y1))false
+        else true
+}
 
 /**
  * Средняя
@@ -35,7 +43,10 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean {
+    return if(r2*r2>=(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+r1*r1)true
+    else false
+}
 
 /**
  * Средняя
@@ -46,4 +57,7 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    return if((a*b<=r*s)||(a*c<=r*s)||(b*c<=r*s))true
+    else false
+}

157b402d31ed70e043405be1a21eec788be8e835
hehe134 <624623296@qq.com>
2016-09-23 15:29:03 +0300

    Решение lesson1 task1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -102,7 +102,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = (initial*pow(percent,3))
+fun accountInThreeYears(initial: Int, percent: Int): Double = (initial * pow(percent*0.01+1,3.0))
 
 /**
  * Простая
@@ -110,4 +110,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = (initial*pow(perce
  * Пользователь задает целое трехзначное число (например, 478).
  *Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = (numbers/100+number/10%10+number%10*100)
+fun numberRevert(number: Int): Int = (number/100 + number/10%10 + number%10 * 100)

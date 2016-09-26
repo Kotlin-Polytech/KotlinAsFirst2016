fdbff0634b80ece1f02df2fe75a5484a24b93591
Albetina3 <2lelandjubila@gmail.com>
2016-09-27 02:31:53 +0300

    I love kotlin

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -17,7 +17,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean= (number / 1000 + (number / 100) % 10) == ((number / 10) % 10 + number % 10)
 
 /**
  * Простая
@@ -25,7 +25,9 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * На шахматной доске стоят два ферзя (ферзь бьет по вертикали, горизонтали и диагоналям).
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int):
+        Boolean =  (x1 == x2) || (y1 == y2) || (abs(x2 - x1) == abs(y2 - y1))
+
 
 /**
  * Средняя
@@ -35,8 +37,8 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
-
+                 x2: Double, y2: Double, r2: Double): Boolean =
+        ((sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) + r1) <= r2)
 /**
  * Средняя
  *
@@ -46,4 +48,5 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean =
+        (((b <= r) && ((a <= s) || (c <= s))) || ((a <= r) && ((b <= s) || (c <= s))) || ((c <= r) && ((a <= s) || (b <= s))))

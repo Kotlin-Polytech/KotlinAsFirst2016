dc526cf441df82f31fcee6c0420dbc45e01744fe
stprost <stprost@users.noreply.github.com>
2016-09-27 21:19:29 +0400

    Update Logical.kt

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -1,8 +1,9 @@
 @file:Suppress("UNUSED_PARAMETER")
 package lesson2.task2
 
+import com.sun.org.apache.xpath.internal.operations.Bool
 import lesson1.task1.sqr
-
+import java.lang.Math.sqrt
 /**
  * Пример
  *
@@ -17,7 +18,14 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val x1 = number%10
+    val x2 = (number%100-x1)/10
+    val x3 = (number%1000-x2*10-x1)/100
+    val x4 = (number-x3*100-x2*10-x1)/1000
+    if (x1+x2==x3+x4) return true
+    else return false
+}
 
 /**
  * Простая
@@ -25,7 +33,12 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * На шахматной доске стоят два ферзя (ферзь бьет по вертикали, горизонтали и диагоналям).
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    if (x1 == x2) return true
+    else if (y1 == y2) return true
+    else if (Math.abs(x1-x2) == Math.abs(y1-y2)) return true
+    else return false
+}
 
 /**
  * Средняя
@@ -35,7 +48,10 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean {
+    if ((sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))+r1) <= r2) return true
+    else return false
+}
 
 /**
  * Средняя

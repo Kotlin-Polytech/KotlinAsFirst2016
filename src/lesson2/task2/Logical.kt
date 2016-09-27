ead739aeb2fddc7cd3763ecc21281ca183545e8e
Kazachenko1998 <kazachenko.1998k@gmail.com>
2016-09-28 00:56:08 +0300

    Lesson2.task2 устранены замечания

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -18,9 +18,9 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean {
-    if ((number % 10 + number / 10 % 10) == (number / 100 % 10 + number / 1000)) return true else return false
-}
+fun isNumberHappy(number: Int): Boolean =
+        if ((number % 10 + number / 10 % 10) == (number / 100 % 10 + number / 1000)) true else false
+
 
 /**
  * Простая
@@ -28,11 +28,8 @@ fun isNumberHappy(number: Int): Boolean {
  * На шахматной доске стоят два ферзя (ферзь бьет по вертикали, горизонтали и диагоналям).
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
-    if ((x1 == x2) || (y2 == y1)) return true
-    else if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) return true
-    else return false
-}
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+        if (((x1 == x2) || (y2 == y1)) || (Math.abs(x1 - x2) == Math.abs(y1 - y2))) true else false
 
 /**
  * Средняя
@@ -42,9 +39,9 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean {
-    if ((Math.sqrt(sqr(x2 - x1) + sqr(y2 - y1)) + r1) <= r2) return true else return false
-}
+                 x2: Double, y2: Double, r2: Double): Boolean =
+        if ((Math.sqrt(sqr(x2 - x1) + sqr(y2 - y1)) + r1) <= r2) true else false
+
 
 /**
  * Средняя
@@ -55,6 +52,11 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
-    if (((a <= r) && (b <= s)) || ((a <= s) && (b <= r)) || ((a <= r) && (c <= s)) || ((a <= s) && (a <= r)) || ((c <= r) && (b <= s)) || ((c <= s) && (b <= r))) return true else return false
-}
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean =
+        if (((a <= r) && (b <= s))
+                || ((a <= s) && (b <= r))
+                || ((a <= r) && (c <= s))
+                || ((a <= s) && (a <= r))
+                || ((c <= r) && (b <= s))
+                || ((c <= s) && (b <= r))) true else false
+

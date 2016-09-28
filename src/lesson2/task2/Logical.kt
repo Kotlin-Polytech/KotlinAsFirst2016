7f414c26fafd4c721c8f2aaebd7ab555151a83a1
VM <vladmkh@rambler.ru>
2016-09-28 13:58:28 +0400

    2 урок

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -17,7 +17,9 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    return ( ( number / 1000) + (number / 100 % 10) == (number / 10 % 10) + (number % 10) )
+}
 
 /**
  * Простая
@@ -25,7 +27,9 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * На шахматной доске стоят два ферзя (ферзь бьет по вертикали, горизонтали и диагоналям).
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    return ( ( x1 == x2 ) || ( y1 == y2 ) || Math.abs( x1 - x2 ) == Math.abs( y1 - y2 ) )
+    }
 
 /**
  * Средняя
@@ -35,7 +39,9 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean {
+    return ( sqr(x2 - x1) + sqr(y2 - y1) <= sqr(r2 - r1) )
+}
 
 /**
  * Средняя

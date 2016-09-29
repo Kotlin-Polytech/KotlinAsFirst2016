8ad72d59fe0497e13bc91dd1a90d0131ed8429af
sasha <fail.incognito.evereday@gmail.com>
2016-09-29 19:58:46 +0300

    Сделала тривиальные, простые и среднии задачи из Lesson3

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -57,7 +57,19 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  * Найти количество цифр в заданном числе n.
  * Например, число 1 содержит 1 цифру, 456 -- 3 цифры, 65536 -- 5 цифр.
  */
-fun digitNumber(n: Int): Int = TODO()
+fun digitNumber(n: Int): Int {
+    var nn: Int = n
+    var count: Int = 0
+    if (nn == 0) return(1)
+    else {
+        while (nn >= 1) {
+            count++
+            nn = nn / 10
+        }
+        return (count)
+    }
+
+}
 
 /**
  * Простая
@@ -65,7 +77,19 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+fun fib(n: Int): Int {
+    var n=n-1
+    var row=listOf(1,1)
+    if (n<2) return 1
+    else {
+        for (i in 2..n) {
+            var x = row[i - 1] + row[i - 2]
+            row += x
+        }
+        var result = row[n]
+        return (result)
+    }
+}
 
 /**
  * Простая
@@ -73,21 +97,33 @@ fun fib(n: Int): Int = TODO()
  * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
  * минимальное число k, которое делится и на m и на n без остатка
  */
-fun lcm(m: Int, n: Int): Int = TODO()
+fun nod(x:Int, y: Int): Int = if (x!= 0) nod(y%x,x) else y
+fun lcm(m: Int, n: Int): Int {
+    var result = m*n/nod(m,n)
+    return(result)
+}
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
-fun minDivisor(n: Int): Int = TODO()
+fun minDivisor(n: Int): Int {
+    var i = 2
+    while(n%i !=0) i++
+    return(i)
+}
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    var i = n-1
+    while(n%i !=0) i--
+    return(i)
+}
 
 /**
  * Простая
@@ -96,7 +132,16 @@ fun maxDivisor(n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+    var result = 0
+    var i=m
+    while(i>1) {
+        if ((m%i==0) and (n%i==0)) result++
+        i--
+    }
+    if (result>0) return(false)
+    else return(true)
+}
 
 /**
  * Простая
@@ -105,7 +150,14 @@ fun isCoPrime(m: Int, n: Int): Boolean = TODO()
  * то есть, существует ли такое целое k, что m <= k*k <= n.
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
-fun squareBetweenExists(m: Int, n: Int): Boolean = TODO()
+fun squareBetweenExists(m: Int, n: Int): Boolean {
+    var x = 0
+    for (i in m..n) {
+        x = Math.sqrt(i.toDouble()).toInt()
+        if (i==x) return true
+    }
+    return false
+}
 
 /**
  * Простая
@@ -114,7 +166,19 @@ fun squareBetweenExists(m: Int, n: Int): Boolean = TODO()
  * sin(x) = x - x^3 / 3! + x^5 / 5! - x^7 / 7! + ...
  * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю
  */
-fun sin(x: Double, eps: Double): Double = TODO()
+fun sin(x: Double, eps: Double): Double {
+    var mark=1
+    var ind=1
+    var sin=0.0
+    var newElement=x
+    while (newElement>eps) {
+        sin+=mark*newElement
+        ind+=2
+        newElement=Math.pow(x,ind.toDouble())/factorial(ind)
+        mark=0-mark
+    }
+    return(sin)
+}
 
 /**
  * Простая
@@ -123,7 +187,19 @@ fun sin(x: Double, eps: Double): Double = TODO()
  * cos(x) = 1 - x^2 / 2! + x^4 / 4! - x^6 / 6! + ...
  * Нужную точность считать достигнутой, если очередной член ряда меньше eps по модулю
  */
-fun cos(x: Double, eps: Double): Double = TODO()
+fun cos(x: Double, eps: Double): Double {
+    var mark=1
+    var ind=0
+    var cos=0.0
+    var newElement=1.0
+    while (newElement>eps) {
+        cos+=mark*newElement
+        ind+=2
+        newElement=Math.pow(x,ind.toDouble())/factorial(ind)
+        mark=0-mark
+    }
+    return(cos)
+}
 
 /**
  * Средняя
@@ -131,7 +207,19 @@ fun cos(x: Double, eps: Double): Double = TODO()
  * Поменять порядок цифр заданного числа n на обратный: 13478 -> 87431.
  * Не использовать строки при решении задачи.
  */
-fun revert(n: Int): Int = TODO()
+fun revert(n: Int): Int {
+    var result=0
+    var nn=n
+    var x=10
+    while (nn%x!=n) x*=10
+    x/=10
+    while (nn>0){
+        result+=(nn%10)*x
+        nn/=10
+        x/=10
+    }
+    return(result)
+}
 
 /**
  * Средняя
@@ -140,15 +228,30 @@ fun revert(n: Int): Int = TODO()
  * первая цифра равна последней, вторая -- предпоследней и так далее.
  * 15751 -- палиндром, 3653 -- нет.
  */
-fun isPalindrome(n: Int): Boolean = TODO()
-
+fun isPalindrome(n: Int): Boolean {
+var n1=n
+var n2=0
+while (n1>0) {
+    n2=n2*10+n1%10
+    n1/=10
+}
+return (n==n2)
+}
 /**
  * Средняя
  *
  * Для заданного числа n определить, содержит ли оно различающиеся цифры.
  * Например, 54 и 323 состоят из разных цифр, а 111 и 0 из одинаковых.
  */
-fun hasDifferentDigits(n: Int): Boolean = TODO()
+fun hasDifferentDigits(n: Int): Boolean {
+    var memory = n % 10
+    var nn = n
+    while (nn > 0) {
+        if (nn % 10 != memory) return true
+        nn /= 10
+    }
+    return false
+}
 
 /**
  * Сложная

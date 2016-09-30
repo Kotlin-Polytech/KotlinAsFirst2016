b05d1366aad35e74415c3c925f1b942eef1183d5
malashik35 <malashik35@mail.ru>
2016-10-01 00:35:30 +0300

    урок 2

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -72,7 +72,7 @@ fun timeForHalfWay(t1: Double, v1: Double,
                 var semiT = ((semiLengthS - t1*v1 - t2*v2) / v3)+t1+t2
                 return semiT
             }
-            return 0.0
+            return  0.0
         }
 
 

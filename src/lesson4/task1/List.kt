fe28a7a1ec9b82345ba8024d899c7b2e2d589e3e
Sidorina_Test1 <elena_petrova98@mail.ru>
2016-10-02 18:43:34 +0300

    Lesson4_task1 Исправила функцию convert.

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -229,6 +229,7 @@ fun factorizeToString(n: Int): String = factorize(n).joinToString("*")
  * например: n = 100, base = 4 -> (1, 2, 1, 0) или n = 250, base = 14 -> (1, 3, 12)
  */
 fun convert(n: Int, base: Int): List<Int> {
+    if (n == 0) return listOf(0)
     var number = n
     var finish = listOf<Int>()
     while (number > 0) {

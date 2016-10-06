7aa06db40ba1c278ffdc552f8e830e583234a12c
Moshkin Alexey <lesha_43rus@mail.ru>
2016-10-06 17:22:08 +0300

    06.10.2016 Lesson6Task1 Почему не работает фунцкия crossPoint?

--- src/lesson6/task1/Geometry.kt
+++ src/lesson6/task1/Geometry.kt
@@ -55,14 +55,19 @@ data class Circle(val center: Point, val radius: Double) {
      * расстояние между их центрами минус сумма их радиусов.
      * Расстояние между пересекающимися окружностями считать равным 0.0.
      */
-    fun distance(other: Circle): Double = TODO()
+    fun distance(other: Circle): Double {
+        val distanceCenter = Math.sqrt(sqr(center.x - other.center.x) + sqr(center.y - other.center.y))
+        val radiusSum = radius + other.radius
+        if (distanceCenter - radiusSum <= 0.0) return 0.0
+        else return distanceCenter - radiusSum
+    }
 
     /**
      * Тривиальная
      *
      * Вернуть true, если и только если окружность содержит данную точку НА себе или ВНУТРИ себя
      */
-    fun contains(p: Point): Boolean = TODO()
+    fun contains(p: Point): Boolean = Math.sqrt(sqr(center.x - p.x) + sqr(center.y - p.y)) <= radius
 }
 
 /**
@@ -76,7 +81,25 @@ data class Segment(val begin: Point, val end: Point)
  * Дано множество точек. Вернуть отрезок, соединяющий две наиболее удалённые из них.
  * Если в множестве менее двух точек, бросить IllegalArgumentException
  */
-fun diameter(vararg points: Point): Segment = TODO()
+fun diameter(vararg points: Point): Segment {
+    if (points.size < 2) throw IllegalArgumentException()
+
+    var maxDiameter = -1.0
+    var maxSegm = Segment(points.first(), points.last())
+
+    if (points.size > 2)
+    for (i in 0..points.size - 2) {
+        for (j in i+1..points.size - 1) {
+            val localDiameter = Math.sqrt(sqr(points[i].x - points[j].x)+sqr(points[i].y - points[j].y))
+            if (localDiameter > maxDiameter) {
+                maxDiameter = localDiameter
+                maxSegm = Segment(points[i], points[j])
+            }
+        }
+    }
+
+    return maxSegm
+}
 
 /**
  * Простая
@@ -84,7 +107,17 @@ fun diameter(vararg points: Point): Segment = TODO()
  * Построить окружность по её диаметру, заданному двумя точками
  * Центр её должен находиться посередине между точками, а радиус составлять половину расстояния между ними
  */
-fun circleByDiameter(diameter: Segment): Circle = TODO()
+fun circleByDiameter(diameter: Segment): Circle {
+    val x1 = diameter.begin.x
+    val x2 = diameter.end.x
+    val y1 = diameter.begin.y
+    val y2 = diameter.end.y
+
+    val radius = Math.sqrt(sqr(x1 - x2) + sqr(y1 - y2))/2.0
+    val x3 = Math.min(x1, x2) + (Math.max(x1, x2) - Math.min(x1, x2))/2.0
+    val y3 = Math.min(y1, y2) + (Math.max(y1, y2) - Math.min(y1, y2))/2.0
+    return Circle(Point(x3, y3), radius)
+}
 
 /**
  * Прямая, заданная точкой и углом наклона (в радианах) по отношению к оси X.
@@ -97,7 +130,26 @@ data class Line(val point: Point, val angle: Double) {
      * Найти точку пересечения с другой линией.
      * Для этого необходимо составить и решить систему из двух уравнений (каждое для своей прямой)
      */
-    fun crossPoint(other: Line): Point = TODO()
+    fun crossPoint(other: Line): Point {
+        val x1 = point.x
+        val y1 = point.y
+        val x2 = other.point.x
+        val y2 = other.point.y
+        val angle2 = other.angle
+        /**
+         *
+         * y - y1 = (x - x1) * tan(angle1)
+         * (x - x1) * tan(angle1) + y1 = (x - x2) * tan(angle2) + y2
+         * x * (tan(angle1) - tan(angle2)) - x1 * tan(angle1) + x2 * tan(angle2) = y2 - y1
+         * x = ( y2 - y1 + x1 * tan(angle1) - x2 * tan(angle2) ) / ( tan(angle1) - tan(angle2) )
+         * y = (x - x1) * tan(angle1) + y1
+         */
+
+        val x3 = (x1*Math.tan(angle) - x2*Math.tan(angle2) + y2 - y1)/(Math.tan(angle) - Math.tan(angle2))
+        val y3 = (x3-x1)*Math.tan(angle) + y1
+
+        return Point(x3, y3)
+    }
 }
 
 /**

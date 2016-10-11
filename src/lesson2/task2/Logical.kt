512ad75d87d8ff5245a76577c03e348bdfbb2410
stprost <stprost@users.noreply.github.com>
2016-10-11 22:24:36 +0400

    Update Logical.kt

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -1,9 +1,11 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson2.task2
 
 import com.sun.org.apache.xpath.internal.operations.Bool
 import lesson1.task1.sqr
 import java.lang.Math.sqrt
+
 /**
  * Пример
  *
@@ -18,14 +20,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean {
-    val x1 = number%10
-    val x2 = (number%100-x1)/10
-    val x3 = (number%1000-x2*10-x1)/100
-    val x4 = (number-x3*100-x2*10-x1)/1000
-    if (x1+x2==x3+x4) return true
-    else return false
-}
+fun isNumberHappy(number: Int): Boolean = number % 10 + number / 10 % 10 == number / 100 % 10 + number / 1000
 
 /**
  * Простая
@@ -36,7 +31,7 @@ fun isNumberHappy(number: Int): Boolean {
 fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
     if (x1 == x2) return true
     else if (y1 == y2) return true
-    else if (Math.abs(x1-x2) == Math.abs(y1-y2)) return true
+    else if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) return true
     else return false
 }
 
@@ -48,18 +43,16 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean {
-    if ((sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))+r1) <= r2) return true
-    else return false
-}
+                 x2: Double, y2: Double, r2: Double): Boolean = (sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) + r1) <= r2
 
-/**
- * Средняя
- *
- * Определить, пройдет ли кирпич со сторонами а, b, c сквозь прямоугольное отверстие в стене со сторонами r и s.
- * Стороны отверстия должны быть параллельны граням кирпича.
- * Считать, что совпадения длин сторон достаточно для прохождения кирпича, т.е., например,
- * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
- * Вернуть true, если кирпич пройдёт
- */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+
+    /**
+     * Средняя
+     *
+     * Определить, пройдет ли кирпич со сторонами а, b, c сквозь прямоугольное отверстие в стене со сторонами r и s.
+     * Стороны отверстия должны быть параллельны граням кирпича.
+     * Считать, что совпадения длин сторон достаточно для прохождения кирпича, т.е., например,
+     * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
+     * Вернуть true, если кирпич пройдёт
+     */
+        fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()

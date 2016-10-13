d873aec124df893b1622b31ce24effa18d07ffa8
Gast9ra <mavrovaisli@gmail.com>
2016-10-13 12:17:50 +0300

    сделано 4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -104,14 +104,30 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double = TODO()
+fun abs(v: List<Double>): Double {
+    var result:Double=0.0
+    for (i in 0..v.size-1){
+        result+=v[i]*v[i]
+    }
+    return (Math.sqrt(result))
+}
 
 /**
  * Простая
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double {
+    if (list.isEmpty()) return 0.0
+    else {
+        val len: Int = list.size
+        var all:Double=0.0
+        for (i in 0..len-1){
+            all+=list[i]
+        }
+        return all/len.toDouble()
+    }
+}
 
 /**
  * Средняя
@@ -119,7 +135,16 @@ fun mean(list: List<Double>): Double = TODO()
  * Центрировать заданный список list, уменьшив каждый элемент на среднее арифметическое всех элементов.
  * Если список пуст, не делать ничего. Вернуть изменённый список.
  */
-fun center(list: MutableList<Double>): MutableList<Double> = TODO()
+fun center(list: MutableList<Double>): MutableList<Double> {
+    if (list.isEmpty()) return list
+    val midlelist:Double=mean(list)
+    for (i in 0..list.size-1){
+        list[i]-=midlelist
+    }
+return list
+}
+
+
 
 /**
  * Средняя
@@ -128,7 +153,16 @@ fun center(list: MutableList<Double>): MutableList<Double> = TODO()
  * представленные в виде списков a и b. Скалярное произведение считать по формуле:
  * C = a1b1 + a2b2 + ... + aNbN. Произведение пустых векторов считать равным 0.0.
  */
-fun times(a: List<Double>, b: List<Double>): Double = TODO()
+fun times(a: List<Double>, b: List<Double>): Double =
+    if (a.isEmpty()&&b.isEmpty())  0.0
+    else {
+        var scalar: Double = 0.0
+        for (i in 0..a.size - 1) {
+            scalar += a[i] * b[i]
+        }
+        scalar
+    }
+
 
 /**
  * Средняя
@@ -138,7 +172,16 @@ fun times(a: List<Double>, b: List<Double>): Double = TODO()
  * Коэффициенты многочлена заданы списком p: (p0, p1, p2, p3, ..., pN).
  * Значение пустого многочлена равно 0.0 при любом x.
  */
-fun polynom(p: List<Double>, x: Double): Double = TODO()
+fun polynom(p: List<Double>, x: Double): Double=
+        if (p.isEmpty()) 0.0
+        else {
+            var sum:Double=0.0
+            for (i in 0..p.size-1){
+                sum+=p[i]*Math.pow(x,i.toDouble())
+            }
+            sum
+        }
+
 
 /**
  * Средняя
@@ -148,7 +191,17 @@ fun polynom(p: List<Double>, x: Double): Double = TODO()
  * Например: 1, 2, 3, 4 -> 1, 3, 6, 10.
  * Пустой список не следует изменять. Вернуть изменённый список.
  */
-fun accumulate(list: MutableList<Double>): MutableList<Double> = TODO()
+fun accumulate(list: MutableList<Double>): MutableList<Double> =
+        if (list.isEmpty()) list
+        else {
+
+            var gap:Double=0.0
+            for (i in 0..list.size-1){
+                gap+=list[i]
+                list[i]=gap
+            }
+            list
+        }
 
 /**
  * Средняя
@@ -157,7 +210,43 @@ fun accumulate(list: MutableList<Double>): MutableList<Double> = TODO()
  * Результат разложения вернуть в виде списка множителей, например 75 -> (3, 5, 5).
  * Множители в списке должны располагаться по возрастанию.
  */
-fun factorize(n: Int): List<Int> = TODO()
+fun factorize(n: Int): List<Int> {
+    val result=mutableListOf<Int>()
+    var n1=n
+    while ((n1!=1)){
+        when{
+            n1%2==0->{ n1/=2
+                result.add(2)
+            }
+            n1%3==0->{ n1/=3
+                result.add(3)
+            }
+            n1%4==0->{ n1/=4
+                result.add(4)
+            }
+            n1%5==0->{n1/=5
+            result.add(5)
+            }
+            n1%6==0->{n1/=6
+                result.add(6)
+            }
+            n1%7==0->{n1/=7
+                result.add(7)
+            }
+            n1%8==0->{n1/=8
+                result.add(8)
+            }
+            n1%9==0->{n1/=9
+                result.add(9)
+            }
+            else->{
+                result.add(n1)
+                return result
+            }
+        }
+    }
+    return result
+}
 
 /**
  * Сложная
@@ -165,7 +254,45 @@ fun factorize(n: Int): List<Int> = TODO()
  * Разложить заданное натуральное число n > 1 на простые множители.
  * Результат разложения вернуть в виде строки, например 75 -> 3*5*5
  */
-fun factorizeToString(n: Int): String = TODO()
+fun factorizeToString(n: Int): String {
+    var result:String=""
+    var n1=n
+    while ((n1!=1)){
+        when{
+            n1%2==0->{ n1/=2
+                result+=("2")
+            }
+            n1%3==0->{ n1/=3
+                result+=("3")
+            }
+            n1%4==0->{ n1/=4
+                result+=("4")
+            }
+            n1%5==0->{n1/=5
+                result+=("5")
+            }
+            n1%6==0->{n1/=6
+                result+=("6")
+            }
+            n1%7==0->{n1/=7
+                result+=("7")
+            }
+            n1%8==0->{n1/=8
+                result+=("8")
+            }
+            n1%9==0->{n1/=9
+                result+=("9")
+            }
+            else->{
+                result+=(n1).toString()
+                return result
+            }
+        }
+        if (n1>1) result+="*"
+    }
+    return result
+}
+
 
 /**
  * Средняя
@@ -174,7 +301,17 @@ fun factorizeToString(n: Int): String = TODO()
  * Результат перевода вернуть в виде списка цифр в base-ичной системе от старшей к младшей,
  * например: n = 100, base = 4 -> (1, 2, 1, 0) или n = 250, base = 14 -> (1, 3, 12)
  */
-fun convert(n: Int, base: Int): List<Int> = TODO()
+fun convert(n: Int, base: Int): List<Int> {
+    var n1: Int = n
+    var list: List<Int>
+    list = listOf()
+    if (n==0) list+=0
+    while (n1>0){
+        list+=n1%base
+        n1/=base
+    }
+    return list.reversed()
+}
 
 /**
  * Сложная
@@ -184,7 +321,17 @@ fun convert(n: Int, base: Int): List<Int> = TODO()
  * строчными буквами: 10 -> a, 11 -> b, 12 -> c и так далее.
  * Например: n = 100, base = 4 -> 1210, n = 250, base = 14 -> 13c
  */
-fun convertToString(n: Int, base: Int): String = TODO()
+fun convertToString(n: Int, base: Int): String {
+    var list: List<Int>
+    var string1: String = ""
+    if (n==0) string1=""
+    list = convert(n, base)
+    for (i in 0..list.size - 1) {
+        if (list[i] > 9) string1+=(87 + list[i]).toChar()
+        else string1+=(list[i]).toString()
+    }
+    return string1
+}
 
 /**
  * Средняя
@@ -193,7 +340,11 @@ fun convertToString(n: Int, base: Int): String = TODO()
  * из системы счисления с основанием base в десятичную.
  * Например: digits = (1, 3, 12), base = 14 -> 250
  */
-fun decimal(digits: List<Int>, base: Int): Int = TODO()
+fun decimal(digits: List<Int>, base: Int): Int {
+    var result: Int = 0
+    for (i in (digits.size-1) downTo 0) result+=(digits[i]*Math.pow(base.toDouble(),((digits.size-1)-i).toDouble())).toInt()
+    return result
+}
 
 /**
  * Сложная
@@ -204,7 +355,16 @@ fun decimal(digits: List<Int>, base: Int): Int = TODO()
  * 10 -> a, 11 -> b, 12 -> c и так далее.
  * Например: str = "13c", base = 14 -> 250
  */
-fun decimalFromString(str: String, base: Int): Int = TODO()
+fun decimalFromString(str: String, base: Int): Int {
+    var list: List<Int>
+    list = listOf()
+    var string1:String=str
+    for (i in 0..string1.length-1) {
+        if (string1[i] in '0'..'9') list+=((string1[i]).toInt()-48)
+        else list+=((string1[i]).toInt()-87)
+    }
+    return decimal(list, base)
+}
 
 /**
  * Сложная
@@ -214,7 +374,90 @@ fun decimalFromString(str: String, base: Int): Int = TODO()
  * 90 = XC, 100 = C, 400 = CD, 500 = D, 900 = CM, 1000 = M.
  * Например: 23 = XXIII, 44 = XLIV, 100 = C
  */
-fun roman(n: Int): String = TODO()
+fun roman(n: Int): String {
+    var n1=n
+    var counter=0
+    var result:String=""
+    while (n1!=0){
+        counter++
+        val num:Int=n1%10
+        when{   //translate number
+            counter==1->{ //блок перевода разряда
+                when{
+                    (num>=1)&&(num<=3)-> {
+                        for (i in 1..num){
+                            result+="I"
+                        }
+                    }
+                    num==4->result+="VI" //число пишется наоборот ,так как идем по числу с конца
+                    num==5->result+="V"
+                    (num>5)&&(num<9)->{
+                        for (i in 1..(num-5)){
+                            result+="I"
+                        }
+                        result+="V"
+                    }
+                    num==9->result+="XI"
+                }
+            }
+            counter==2->{//блок перевода разряда
+                when{
+                    (num>=1)&&(num<=3)-> {
+                        for (i in 1..num){
+                            result+="X"
+                        }
+                    }
+                    num==4->result+="LX"
+                    num==5->result+="L"
+                    (num>5)&&(num<9)->{
+                        for (i in 1..(num-5)){
+                            result+="X"
+                        }
+                        result+="L"
+                    }
+                    num==9->result+="CX"
+                }
+
+            }
+            counter==3->{//блок перевода разряда
+                when{
+                    (num>=1)&&(num<=3)-> {
+                        for (i in 1..num){
+                            result+="C"
+                        }
+                    }
+                    num==4->result+="DC"
+                    num==5->result+="D"
+                    (num>5)&&(num<9)->{
+                        for (i in 1..(num-5)){
+                            result+="C"
+                        }
+                        result+="D"
+                    }
+                    num==9->result+="MC"
+                }
+
+            }
+            counter==4->{//блок перевода разряда
+                if ((num>=1)&&(num<=3)) {
+                    for (i in 1..num){
+                        result+="M"
+                    }
+                }
+            }
+            else->{
+                println("error")
+                n1=0
+            }
+        }
+        n1/=10
+    }
+    return result.reversed()
+}
+
+
+
+
 
 /**
  * Очень сложная

da5ef5432e3c76efd5a0af9a8480ee3f8be73b99
Kate <ekat.pronina@gmail.com>
2016-10-16 11:03:36 +0300

    Lesson 2. Both  parts.

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -1,7 +1,10 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson2.task2
 
+import lesson1.task1.sqRoot
 import lesson1.task1.sqr
+import lesson4.task1.sqRoots
 
 /**
  * Пример
@@ -17,7 +20,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean = ((number / 1000) + (number / 100 % 10)) == ((number / 10 % 10) + (number % 10))
 
 /**
  * Простая
@@ -34,8 +37,10 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * окружности с центром в (x2, y2) и радиусом r2.
  * Вернуть true, если утверждение верно
  */
+
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean = sqrt(sqr(x2 - x1) + sqr(y2 - y1))<= (r2 - r1)
+
 
 /**
  * Средняя
@@ -46,4 +51,10 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    val k1 = ((a <= r) && ((b <= s) || (c <= s)))
+    val k2 = ((b <= r) && ((a <= s) || (c <= s)))
+    val k3 = ((c <= r) && ((a <= s) || (b <= s)))
+
+    return (k1 || k2 || k3)
+}

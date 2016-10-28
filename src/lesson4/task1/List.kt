ce3a23e75a119a88cc49c25f6dc16f6879ff7b0d
copchenka <vbn366203>
2016-10-29 00:11:29 +0300

    4 урок

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -227,7 +227,7 @@ fun convert(n: Int, base: Int): List<Int> {
  * строчными буквами: 10 -> a, 11 -> b, 12 -> c и так далее.
  * Например: n = 100, base = 4 -> 1210, n = 250, base = 14 -> 13c
  */
-fun convertToString(n: Int, base: Int): String = TODO()
+fun convertToString(n: Int, base: Int): String =  TODO()
 
 /**
  * Средняя

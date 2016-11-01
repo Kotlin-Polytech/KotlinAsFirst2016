a8cda79519445f499635f773a0d1c23f0d8023fb
Sidorina_Test1 <elena_petrova98@mail.ru>
2016-11-01 20:39:48 +0300

    Lesson6_task1 Исправила незначительные замечания по данному таску.

--- src/lesson6/task1/Geometry.kt
+++ src/lesson6/task1/Geometry.kt
@@ -86,7 +86,7 @@ fun diameter(vararg points: Point): Segment {
     var maxdist = -1.0
     var adress = Segment(points[0], points[0])
     for (i in 0..points.size - 2) {
-        for (k in 1..points.size - 1) {
+        for (k in i+1..points.size - 1) {
             val curdist = points[i].distance(points[k])
             if (curdist > maxdist) {
                 maxdist = curdist
@@ -119,14 +119,14 @@ data class Line(val point: Point, val angle: Double) {
      * Для этого необходимо составить и решить систему из двух уравнений (каждое для своей прямой)
      */
     fun crossPoint(other: Line): Point {
-        val A1 = -Math.sin(angle)
-        val B1 = Math.cos(angle)
-        val C1 = -A1 * point.x - B1 * point.y
-        val A2 = -Math.sin(other.angle)
-        val B2 = Math.cos(other.angle)
-        val C2 = -A2 * other.point.x - B2 * other.point.y
-        val low = A1 * B2 - A2 * B1
-        return Point((B1 * C2 - B2 * C1) / low, (C1 * A2 - C2 * A1) / low)
+        val a1 = -Math.sin(angle)
+        val b1 = Math.cos(angle)
+        val c1 = -a1 * point.x - b1 * point.y
+        val a2 = -Math.sin(other.angle)
+        val b2 = Math.cos(other.angle)
+        val c2 = -a2 * other.point.x - b2 * other.point.y
+        val low = a1 * b2 - a2 * b1
+        return Point((b1 * c2 - b2 * c1) / low, (c1 * a2 - c2 * a1) / low)
     }
 }
 
@@ -138,7 +138,7 @@ data class Line(val point: Point, val angle: Double) {
 fun lineBySegment(s: Segment): Line {
     var angle = 0.0
     if (s.begin != s.end) {
-        angle = Math.atan((s.end.y - s.begin.y) / (s.end.x - s.begin.x))
+        angle = Math.atan2((s.end.y - s.begin.y), (s.end.x - s.begin.x))
     }
     return Line(Point(s.begin.x, s.begin.y), angle)
 }
@@ -159,7 +159,7 @@ fun bisectorByPoints(a: Point, b: Point): Line {
     val circ: Circle = circleByDiameter(Segment(a, b))
     val a1 = (b.x - circ.center.x)
     val b1 = (circ.center.y - b.y)
-    return Line(Point(circ.center.x, circ.center.y), Math.atan(a1 / (b1 + Double.MIN_VALUE)))
+    return Line(Point(circ.center.x, circ.center.y), Math.atan2(a1, b1))
 }
 
 /**
@@ -181,9 +181,10 @@ fun findNearestCirclePair(vararg circles: Circle): Pair<Circle, Circle> = TODO()
  */
 fun circleByThreePoints(a: Point, b: Point, c: Point): Circle {
     //проверяем, существует ли треугольник, построенный по заданным точкам
-    if (a.distance(b) > a.distance(c) + b.distance(c) ||
-            a.distance(c) > a.distance(b) + b.distance(c) ||
-            b.distance(c) > a.distance(b) + a.distance(c))
+    val ab = a.distance(b)
+    val ac = a.distance(c)
+    val bc = b.distance(c)
+    if (ab > ac + bc || ac > ab + bc || bc > ab + ac)
         throw IllegalArgumentException("Треугольник не существует")
     val center = bisectorByPoints(a, b).crossPoint(bisectorByPoints(b, c))
     return Circle(center, center.distance(a))

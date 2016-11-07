b8c3a0adff117f7c9d14c2e0f376e6ea7e74d501
NanXiang <xjxllwj116@gmai.com>
2016-11-07 21:57:48 +0300

    change lesson 3

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -160,11 +160,8 @@ fun gcd(m: Int, n: Int): Int {
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean {
-    gcd(m, n)
-    if (m==1)return true
-    else return false
-}
+fun isCoPrime(m: Int, n: Int): Boolean = gcd(m, n) == 1
+
 
 /**
  * Простая

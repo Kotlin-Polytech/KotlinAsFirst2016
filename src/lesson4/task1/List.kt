56ff102ff4ce21f4030d034d1f555b40449336e9
Anastasiiachu <chupanastasia@gmail.com>
2016-11-10 13:29:03 +0300

    4_lesson

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -227,7 +227,8 @@ fun factorizeToString(n: Int): String = factorize(n).joinToString(separator = "*
 fun convert(n: Int, base: Int): List<Int> {
     var number = n
     val list = mutableListOf<Int>()
-    while (Math.abs(number) > 0) {
+    if (number == 0) return listOf(0)
+    while (number != 0) {
         val result = number % base
         number /= base
         list.add(result)

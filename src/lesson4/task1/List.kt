16e4972e1b1140ead5808e853a98978587ee7362
Nikolay123456789 <nikoscharits@yahoo.com>
2016-11-10 15:15:10 +0300

    lesson 4. Task 1

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -213,7 +213,7 @@ fun factorize(n: Int): List<Int> {
  * Разложить заданное натуральное число n > 1 на простые множители.
  * Результат разложения вернуть в виде строки, например 75 -> 3*5*5
  */
-fun factorizeToString(n: Int): String = TODO()
+fun factorizeToString(n: Int): String = factorize(n).joinToString(separator = "*")
 
 /**
  * Средняя
@@ -222,7 +222,16 @@ fun factorizeToString(n: Int): String = TODO()
  * Результат перевода вернуть в виде списка цифр в base-ичной системе от старшей к младшей,
  * например: n = 100, base = 4 -> (1, 2, 1, 0) или n = 250, base = 14 -> (1, 3, 12)
  */
-fun convert(n: Int, base: Int): List<Int> = TODO()
+fun convert(n: Int, base: Int): List<Int> {
+    var number = n
+    val result = mutableListOf<Int>()
+    if (number == 0) result.add(0)
+    while (number > 0) {
+        result.add(0, number % base)
+        number = number / base
+    }
+    return result
+}
 
 /**
  * Сложная

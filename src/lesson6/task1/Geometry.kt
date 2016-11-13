a586fa7f0c74dba068acfa0ccc336c139d8f8f8f
Илья <ilya.dolgushev@yandex.ru>
2016-11-13 17:04:51 +0300

    start lesson 6 (task1)

--- src/lesson6/task1/Geometry.kt
+++ src/lesson6/task1/Geometry.kt
@@ -83,16 +83,16 @@ data class Segment(val begin: Point, val end: Point)
  * Если в множестве менее двух точек, бросить IllegalArgumentException
  */
 fun diameter(vararg points: Point): Segment {
-    var distance = 0.0
     if (points.size < 2) throw IllegalArgumentException()
     var p1 = points[0]
     var p2 = points[1]
-    for (i in 0..points.size - 2) {
+    var distance = p1.distance(p2)
+    for (i in 1..points.size - 2) {
         for (k in i..points.size - 2) {
             p1 = points[k]
             p2 = points[k + 1]
-            val segment = p1.distance(p2)
-            if (segment > distance) distance = segment
+            val lengthSegment = p1.distance(p2)
+            if (lengthSegment > distance) distance = lengthSegment
         }
     }
     return Segment(p1, p2)
@@ -147,7 +147,15 @@ fun lineByPoints(a: Point, b: Point) = Line(a, atan((b.y - a.y) / (b.x - a.x)))
  *
  * Построить серединный перпендикуляр по отрезку или по двум точкам
  */
-fun bisectorByPoints(a: Point, b: Point): Line = TODO()
+fun bisectorByPoints(a: Point, b: Point): Line {
+    val halfSegment = Point((a.x + b.x) / 2, (a.y + b.y) / 2)
+    return when{
+        a.y==b.y -> lineByPoints(halfSegment, Point(halfSegment.x,halfSegment.y+1))
+        a.x==b.x -> lineByPoints(halfSegment,Point(halfSegment.x+1,halfSegment.y))
+        else -> {val pointBisector = Point(halfSegment.x + 1, sqr(b.y) - sqr(a.y) + sqr(b.x) - sqr(a.x) - 2 * (halfSegment.x + 1) * (a.x - b.x) / 2 * (b.y - a.y))
+        return lineByPoints(halfSegment, pointBisector)}
+    }
+}
 
 /**
  * Средняя
@@ -166,7 +174,13 @@ fun findNearestCirclePair(vararg circles: Circle): Pair<Circle, Circle> = TODO()
  * (построить окружность по трём точкам, или
  * построить окружность, описанную вокруг треугольника - эквивалентная задача).
  */
-fun circleByThreePoints(a: Point, b: Point, c: Point): Circle = TODO()
+fun circleByThreePoints(a: Point, b: Point, c: Point): Circle {
+    val line1 = bisectorByPoints(a,b)
+    val line2 = bisectorByPoints(b,c)
+    val center = line1.crossPoint(line2)
+    val radius = center.distance(a)
+    return Circle(center, radius)
+}
 
 /**
  * Очень сложная

34d4ad0dbe3e48825f72ba4d651ac48de45cd9e1
prokhorovmax <prokhorovmaxim2009@yandex.ru>
2016-11-21 16:24:36 +0400

    l6 t1 - crossPoint(normal)

--- src/lesson6/task1/Geometry.kt
+++ src/lesson6/task1/Geometry.kt
@@ -125,7 +125,11 @@ data class Line(val point: Point, val angle: Double) {
      * Найти точку пересечения с другой линией.
      * Для этого необходимо составить и решить систему из двух уравнений (каждое для своей прямой)
      */
-    fun crossPoint(other: Line): Point = TODO()
+    fun crossPoint(other: Line): Point {
+        val x = (this.point.x * Math.tan(this.angle) - other.point.x * Math.tan(other.angle) + other.point.y - this.point.y) / (Math.tan(this.angle) - Math.tan(other.angle))
+        val y = (x - other.point.x) * Math.tan(other.angle) + other.point.y
+        return Point(x, y)
+    }
 }
 
 /**

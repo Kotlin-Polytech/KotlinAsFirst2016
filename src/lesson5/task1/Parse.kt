ff6202ba6a9d16ebc5fd9c5accd7c13d0a801b77
AlexanderKuptsov <shon55398@gmail.com>
2016-11-22 02:25:54 +0300

    fixes

--- src/lesson5/task1/Parse.kt
+++ src/lesson5/task1/Parse.kt
@@ -196,14 +196,13 @@ fun bestHighJump(jumps: String): Int {
 fun plusMinus(expression: String): Int {
     var res = 0
     if (expression.contains(Regex("""[^-+\d\s]""")) || expression == "") throw IllegalArgumentException()
-    if (expression.contains(Regex("""[-+]"""))) Double.NaN
-    else if (expression.contains(Regex("""[\s]"""))) throw IllegalArgumentException()
+    if (expression.contains(Regex("""[-+]"""))) {
+    } else if (expression.contains(Regex("""[\s]"""))) throw IllegalArgumentException()
     else return expression.toInt()
-    val symbols = Regex("""[^+-]""").replace(expression, "")
-    val parts = Regex("""[\s]""").replace(expression, "").split("+", "-")
+    val parts = expression.trim().split(" ")
     res += parts.first().toInt()
-    for (i in 1..parts.size - 1) {
-        if (symbols[i - 1] == '+') res += parts[i].toInt() else res -= parts[i].toInt()
+    for (i in 1..parts.size - 1 step 2) {
+        if (parts[i] == "+") res += parts[i + 1].toInt() else res -= parts[i + 1].toInt()
     }
     return res
 }

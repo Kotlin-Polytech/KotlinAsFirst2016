f3d518ffa4a7a3719adc36905b354b7b689b3d6c
Ilya Shavkunov <shavkunov1198@gmail.com>
2016-11-22 19:53:14 +0300

    Исправил ошибки замеченые в Lesson4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -121,7 +121,16 @@ fun mean(list: List<Double>): Double = if (list.size == 0) 0.0 else list.sum() /
  * Центрировать заданный список list, уменьшив каждый элемент на среднее арифметическое всех элементов.
  * Если список пуст, не делать ничего. Вернуть изменённый список.
  */
-fun center(list: MutableList<Double>): MutableList<Double> = list.map { it - mean(list) }.toMutableList()
+fun center(list: MutableList<Double>): MutableList<Double>{
+    if (list.isEmpty()) return list
+    val mid = mean(list)
+    for (i in 0..list.size - 1) {
+        list[i] -= mid
+    }
+    return list
+}
+
+
 
 /**
  * Средняя
@@ -145,10 +154,14 @@ fun times(a: List<Double>, b: List<Double>): Double {
  * Значение пустого многочлена равно 0.0 при любом x.
  */
 fun polynom(p: List<Double>, x: Double): Double {
-    var length = 0.0
-    for (i in 0..p.size - 1) length += p[i] * Math.pow(x, i.toDouble())
-    return length
+    if (p.isEmpty()) return 0.0
+    var px = p[0]
+    for (i in 1..p.size-1) {
+        px += p[i]*Math.pow(x,i.toDouble())
+    }
+    return px
 }
+
 
 /**
  * Средняя
@@ -174,7 +187,21 @@ fun accumulate(list: MutableList<Double>): MutableList<Double> {
  * Результат разложения вернуть в виде списка множителей, например 75 -> (3, 5, 5).
  * Множители в списке должны располагаться по возрастанию.
  */
-fun factorize(n: Int): List<Int> = TODO()
+fun factorize(n: Int): List<Int> {
+    var list = listOf<Int>()
+    var numb = n
+    for (i in 1..n) {
+        if (isPrime(i) == true) {
+            while (numb % i == 0) {
+                list += i
+                numb /= i
+            }
+        }
+    }
+    return list
+}
+
+
 
 
 /**
@@ -183,11 +210,8 @@ fun factorize(n: Int): List<Int> = TODO()
  * Разложить заданное натуральное число n > 1 на простые множители.
  * Результат разложения вернуть в виде строки, например 75 -> 3*5*5
  */
-fun factorizeToString(n: Int): String {
-    var list: List<Int>
-    list = factorize(n)
-    return list.joinToString("*")
-}
+fun factorizeToString(n: Int): String = factorize(n).joinToString("*")
+
 
 /**
  * Средняя
@@ -197,15 +221,13 @@ fun factorizeToString(n: Int): String {
  * например: n = 100, base = 4 -> (1, 2, 1, 0) или n = 250, base = 14 -> (1, 3, 12)
  */
 fun convert(n: Int, base: Int): List<Int> {
-    var nn = n
-    var list: List<Int>
-    list = listOf()
-    if (n == 0) list += 0
-    while (nn > 0) {
-        list += nn % base
-        nn /= base
+    var number = n
+    var finish = listOf<Int>()
+    while (number > 0) {
+        finish += number % base
+        number /= base
     }
-    return list.reversed()
+    return finish.reversed()
 }
 
 /**
@@ -251,14 +273,20 @@ fun decimal(digits: List<Int>, base: Int): Int {
  * Например: str = "13c", base = 14 -> 250
  */
 fun decimalFromString(str: String, base: Int): Int {
-    var list: List<Int>
-    list = listOf()
-    var string1 = str
-    for (i in 0..string1.length - 1)
-        if (string1[i] in '0'..'9') list += ((string1[i]).toInt() - 48)
-        else list += ((string1[i]).toInt() - 87)
-    return decimal(list, base)
+    var mn = 1
+    var finish = 0
+    for (i in str.length - 1 downTo 0) {
+        if (str[i].toInt() <= '9'.toInt()) {
+            finish += (str[i].toInt() - '0'.toInt()) * mn
+        } else {
+            finish += (str[i].toInt() - ('a'.toInt() - 10)) * mn
+        }
+        mn *= base
+    }
+    return finish
 }
+
+
 
 /**
  * Сложная
@@ -268,7 +296,85 @@ fun decimalFromString(str: String, base: Int): Int {
  * 90 = XC, 100 = C, 400 = CD, 500 = D, 900 = CM, 1000 = M.
  * Например: 23 = XXIII, 44 = XLIV, 100 = C
  */
-fun roman(n: Int): String = TODO()
+fun roman(n: Int): String {
+    var result: String = ""
+    var nn = n
+    var thousands = 0
+    var hundreds = 0
+    var dozens = 0
+    var units = 0
+
+    while (nn >= 1) {
+        if (nn > 1000) {
+            while (nn >= 1000) {
+                thousands = nn / 1000
+                nn = nn % 1000
+
+            }
+        }
+        if ((nn < 1000) && (nn >= 100)) {
+            while (nn >= 100) {
+                hundreds = nn / 100
+                nn = nn % 100
+            }
+        }
+        if ((nn < 100) && (nn >= 10)) {
+            while (nn >= 10) {
+                dozens = nn / 10
+                nn = nn % 10
+
+            }
+        }
+        if (nn < 10) {
+            units = nn
+            nn = 0
+
+        }
+
+
+    }
+    when {
+        (thousands == 1) -> result += ("M")
+        (thousands == 2) -> result += ("MM")
+        (thousands == 3) -> result += ("MMM")
+    }
+    when {
+        (hundreds == 1) -> result += ("C")
+        (hundreds == 2) -> result += ("CC")
+        (hundreds == 3) -> result += ("CCC")
+        (hundreds == 4) -> result += ("CD")
+        (hundreds == 5) -> result += ("D")
+        (hundreds == 6) -> result += ("DC")
+        (hundreds == 7) -> result += ("DCC")
+        (hundreds == 8) -> result += ("DCCC")
+        (hundreds == 9) -> result += ("CM")
+    }
+    when {
+        (dozens == 1) -> result += ("X")
+        (dozens == 2) -> result += ("XX")
+        (dozens == 3) -> result += ("XXX")
+        (dozens == 4) -> result += ("XL")
+        (dozens == 5) -> result += ("L")
+        (dozens == 6) -> result += ("LX")
+        (dozens == 7) -> result += ("LXX")
+        (dozens == 8) -> result += ("LXXX")
+        (dozens == 9) -> result += ("XC")
+    }
+    when {
+        (units == 1) -> result += ("I")
+        (units == 2) -> result += ("II")
+        (units == 3) -> result += ("III")
+        (units == 4) -> result += ("IV")
+        (units == 5) -> result += ("V")
+        (units == 6) -> result += ("VI")
+        (units == 7) -> result += ("VII")
+        (units == 8) -> result += ("VIII")
+        (units == 9) -> result += ("IX")
+    }
+    return result
+}
+
+
 
 /**
  * Очень сложная
@@ -277,4 +383,138 @@ fun roman(n: Int): String = TODO()
  * Например, 375 = "триста семьдесят пять",
  * 23964 = "двадцать три тысячи девятьсот шестьдесят четыре"
  */
-fun russian(n: Int): String = TODO()
+fun russian(n: Int): String {
+    var result: String = ""
+    var thousands100 = 0
+    var thousands10 = 0
+    var thousands = 0
+    var hundreds = 0
+    var dozens = 0
+    var units = 0
+    var nn = n
+    while (nn > -1) {
+        if (nn > 100000) {
+            thousands100 = nn / 100000
+            nn = nn % 100000
+        }
+        if ((nn < 100000) && (nn > 10000)) {
+            thousands10 = nn / 10000
+            nn = nn % 10000
+        }
+        if ((nn < 10000) && (nn > 1000)) {
+            thousands = nn / 1000
+            nn = nn % 1000
+        }
+        if ((nn < 1000) && (nn > 100)) {
+            hundreds = nn / 100
+            nn = nn % 100
+        }
+        if ((nn < 100) && (nn > 10)) {
+            dozens = nn / 10
+            nn = nn % 10
+        }
+        if (nn < 10) {
+            units = nn
+            nn = -1
+        }
+    }
+    when {
+        (thousands100 == 1) -> result += "сто"
+        (thousands100 == 2) -> result += "двести"
+        (thousands100 == 3) -> result += "триста"
+        (thousands100 == 4) -> result += "четыреста"
+        (thousands100 == 5) -> result += "пятьсот"
+        (thousands100 == 6) -> result += "шестьсот"
+        (thousands100 == 7) -> result += "семьсот"
+        (thousands100 == 8) -> result += "восемьсот"
+        (thousands100 == 9) -> result += "девятьсот"
+    }
+    if ((thousands10 != 0) && (thousands100 != 0)) result += " "
+    when {
+        ((thousands10 == 1) && (thousands == 1)) -> result += "одиннадцать"
+        ((thousands10 == 1) && (thousands == 2)) -> result += "двенадцать"
+        ((thousands10 == 1) && (thousands == 3)) -> result += "тринадцать"
+        ((thousands10 == 1) && (thousands == 4)) -> result += "четырнадцать"
+        ((thousands10 == 1) && (thousands == 5)) -> result += "пятнадцать"
+        ((thousands10 == 1) && (thousands == 6)) -> result += "шестнадцать"
+        ((thousands10 == 1) && (thousands == 7)) -> result += "семнадцать"
+        ((thousands10 == 1) && (thousands == 8)) -> result += "восемнадцать"
+        ((thousands10 == 1) && (thousands == 9)) -> result += "девятнадцать"
+        (thousands10 == 2) -> result += "двадцать"
+        (thousands10 == 3) -> result += "тридцать"
+        (thousands10 == 4) -> result += "сорок"
+        (thousands10 == 5) -> result += "пятьдесят"
+        (thousands10 == 6) -> result += "шестьдесят"
+        (thousands10 == 7) -> result += "семьдесят"
+        (thousands10 == 8) -> result += "восемьдесят"
+        (thousands10 == 9) -> result += "девяносто"
+    }
+    if ((thousands != 0) && (thousands10 != 1) && ((thousands100 != 0) || (thousands10 != 0))) result += " "
+    when {
+        ((thousands10 != 1) && (thousands == 1)) -> result += "одна"
+        ((thousands10 != 1) && (thousands == 2)) -> result += "две"
+        ((thousands10 != 1) && (thousands == 3)) -> result += "три"
+        ((thousands10 != 1) && (thousands == 4)) -> result += "четыре"
+        ((thousands10 != 1) && (thousands == 5)) -> result += "пять"
+        ((thousands10 != 1) && (thousands == 6)) -> result += "шесть"
+        ((thousands10 != 1) && (thousands == 7)) -> result += "семь"
+        ((thousands10 != 1) && (thousands == 8)) -> result += "восемь"
+        ((thousands10 != 1) && (thousands == 9)) -> result += "девять"
+    }
+    if ((thousands100 != 0) || (thousands10 != 0) || (thousands != 0)) {
+        if (thousands == 1) {
+            result += " тысяча"
+        } else
+            if ((thousands == 2) || (thousands == 3) || (thousands == 4)) {
+                result += " тысячи"
+            } else
+                result += " тысяч"
+    }
+    if ((hundreds != 0) && ((thousands != 0) || (thousands100 != 0) || (thousands10 != 0))) result += " "
+    when {
+        (hundreds == 1) -> result += "сто"
+        (hundreds == 2) -> result += "двести"
+        (hundreds == 3) -> result += "триста"
+        (hundreds == 4) -> result += "четыреста"
+        (hundreds == 5) -> result += "пятьсот"
+        (hundreds == 6) -> result += "шестьсот"
+        (hundreds == 7) -> result += "семьсот"
+        (hundreds == 8) -> result += "восемьсот"
+        (hundreds == 9) -> result += "девятьсот"
+    }
+    if ((dozens != 0) && ((hundreds != 0) || (thousands != 0) || (thousands100 != 0) || (thousands10 != 0))) result += " "
+    when {
+        ((dozens == 1) && (units == 1)) -> result += "одиннадцать"
+        ((dozens == 1) && (units == 2)) -> result += "двенадцать"
+        ((dozens == 1) && (units == 3)) -> result += "тринадцать"
+        ((dozens == 1) && (units == 4)) -> result += "четырнадцать"
+        ((dozens == 1) && (units == 5)) -> result += "пятнадцать"
+        ((dozens == 1) && (units == 6)) -> result += "шестнадцать"
+        ((dozens == 1) && (units == 7)) -> result += "семнадцать"
+        ((dozens == 1) && (units == 8)) -> result += "восемнадцать"
+        ((dozens == 1) && (units == 9)) -> result += "девятнадцать"
+        (dozens == 2) -> result += "двадцать"
+        (dozens == 3) -> result += "тридцать"
+        (dozens == 4) -> result += "сорок"
+        (dozens == 5) -> result += "пятьдесят"
+        (dozens == 6) -> result += "шестьдесят"
+        (dozens == 7) -> result += "семьдесят"
+        (dozens == 8) -> result += "восемьдесят"
+        (dozens == 9) -> result += "девяносто"
+    }
+    if ((units != 0) && (dozens != 1) && ((dozens != 0) || (hundreds != 0) || (thousands != 0) || (thousands100 != 0) || (thousands10 != 0))) result += " "
+    when {
+        ((dozens != 1) && (units == 1)) -> result += "один"
+        ((dozens != 1) && (units == 2)) -> result += "два"
+        ((dozens != 1) && (units == 3)) -> result += "три"
+        ((dozens != 1) && (units == 4)) -> result += "четыре"
+        ((dozens != 1) && (units == 5)) -> result += "пять"
+        ((dozens != 1) && (units == 6)) -> result += "шесть"
+        ((dozens != 1) && (units == 7)) -> result += "семь"
+        ((dozens != 1) && (units == 8)) -> result += "восемь"
+        ((dozens != 1) && (units == 9)) -> result += "девять"
+    }
+
+    return result
+}
+

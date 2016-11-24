0734c64aef67dc58060c10bbcdbfb04d6e9e5ed0
oddishwolf <bigbossdima98@mail.ru>
2016-11-24 23:35:11 +0300

    Проверка правильности программы через бота

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -58,7 +58,9 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  * Найти количество цифр в заданном числе n.
  * Например, число 1 содержит 1 цифру, 456 -- 3 цифры, 65536 -- 5 цифр.
  */
-fun digitNumber(n: Int): Int = TODO()
+fun digitNumber(n: Int): Int {
+
+}
 
 /**
  * Простая

edb911f5a0805fa6fda8ea075a7c22499de453be
Кирилл <Kulagin1999@list.ru>
2016-11-26 00:09:24 +0400

    changes

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -18,8 +18,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
 fun isNumberHappy(number: Int): Boolean {
-    if ((number / 1000 + (number / 100) % 10) == (number % 10 + (number % 100) / 10)) return true
-    else return false
+    return (number / 1000 + (number / 100) % 10) == (number % 10 + (number % 100) / 10)
 }
 
 /**
@@ -29,8 +28,7 @@ fun isNumberHappy(number: Int): Boolean {
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  */
 fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
-    if ((x1 == x2) || (y1 == y2) || ((x1 - x2) * (x1 - x2) == (y1 - y2) * (y1 - y2))) return true
-    return false
+    return (x1 == x2) || (y1 == y2) || ((x1 - x2) * (x1 - x2) == (y1 - y2) * (y1 - y2))
 }
 /**
  * Средняя
@@ -52,7 +50,6 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * Вернуть true, если кирпич пройдёт
  */
 fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
-    if (((a <= r) &&  (b <= s)) || ((a <= r) && (c <= s)) || ((b <= r) && (a <= s)) || ((b <= r) && (c <= s)) || ((c <= r) && (b <= s)) || ((c <= r) && (a <= s))) return true
-    else return false
+    return ((a <= r) &&  (b <= s)) || ((a <= r) && (c <= s)) || ((b <= r) && (a <= s)) || ((b <= r) && (c <= s)) || ((c <= r) && (b <= s)) || ((c <= r) && (a <= s))
 }
 

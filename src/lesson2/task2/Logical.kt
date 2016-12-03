7b2078e787eca90ca4e772b91a900e59aaae6183
Кирилл <Kulagin1999@list.ru>
2016-12-03 21:17:19 +0400

    Change last 2 tests in 2 lesson task 2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -38,7 +38,10 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean {
+    if (Math.sqrt(sqr(x2 - x1) + sqr(y2 - y1)) + r1 <= r2) return true
+    else return false
+}
 
 /**
  * Средняя

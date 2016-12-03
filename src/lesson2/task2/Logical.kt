4123ae3a7fd383e5cedcce4ad51f3f6a2991ef9f
copchenka <mandragora2319@mail.ru>
2016-12-04 00:26:09 +0300

    Update Logical.kt

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -31,7 +31,7 @@ fun isNumberHappy(number: Int): Boolean {
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  */
 fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
-return (x1 == x2) || (y1 == y2) || (x1 + y1 == x2 + y2) || (x1 - y1 == x2 -y2)
+    return (x1 == x2) || (y1 == y2) || (x1 + y1 == x2 + y2) || (x1 - y1 == x2 - y2)
 }
 
 /**

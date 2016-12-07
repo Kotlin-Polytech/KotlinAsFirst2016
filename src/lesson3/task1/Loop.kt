420d286c560f9119d3f619a05f81d5029207d5e8
Nikolay123456789 <nikoscharits@yahoo.com>
2016-12-07 15:48:34 +0300

    Lesson3.Task1

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -94,7 +94,7 @@ fun fib(n: Int): Int {
  * минимальное число k, которое делится и на m и на n без остатка
  */
 fun lcm(m: Int, n: Int): Int {
-   return (m * n)/NOD(m,n)
+   return (m * NOD(m,n)) / n
 }
 
 /**

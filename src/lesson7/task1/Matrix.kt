b3873bb4e90138665269f089fdc012514c00d9ce
buithuynhung <buithuynhung1997@gmail.com>
2016-12-15 12:42:50 +0300

    Исправерка

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -91,6 +91,7 @@ class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : M
             sb.append("[")
             for (column in 0..width - 1) {
                 sb.append(this[row, column])
+                sb.append(" ")
             }
             sb.append("]\n")
         }

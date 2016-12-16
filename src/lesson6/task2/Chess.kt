9eb968f9b303052a7b8cb4e7a48640f15009e11a
NanXiang <xjxllwj116@gmai.com>
2016-12-16 12:33:31 +0300

    lesson 6 task2 bishopMoveNumber

--- src/lesson6/task2/Chess.kt
+++ src/lesson6/task2/Chess.kt
@@ -124,8 +124,8 @@ fun rookTrajectory(start: Square, end: Square): List<Square> {
  */
 fun bishopMoveNumber(start: Square, end: Square): Int {
     when {
-        (start == end) -> return 0
         (!start.inside() || !end.inside() || start.column == 0 || start.row == 0 || end.column == 0 || end.row == 0) -> throw  IllegalArgumentException()
+        (start == end) -> return 0
         (start.column - end.column) == (start.row - end.row) || (start.column - end.column) == -(start.row - end.row) -> return 1
         (start.column % 2) == (end.column % 2) && (start.row % 2) != (end.row % 2) -> return -1
         (start.column % 2) != (end.column % 2) && (start.row % 2) == (end.row % 2) -> return -1

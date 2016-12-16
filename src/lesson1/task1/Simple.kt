908de573f3684521782531ac2cc67299639f9567
buithuynhung <buithuynhung1997@gmail.com>
2016-12-17 02:45:38 +0300

    Исправерка

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -34,7 +34,6 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
     val sd = sqrt(discriminant(a, b, c))
     val x1 = (-b + sd) / (2 * a)
     val x2 = (-b - sd) / (2 * a)
-
     return x1 * x2 // Результат
 }
 

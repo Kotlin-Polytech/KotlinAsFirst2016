60bc8abc5761bbe1b993ee66c7ff01afca4d73ac
copchenka <mandragora2319@mail.ru>
2016-12-19 00:34:52 +0300

    Update IfElse.kt

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -35,7 +35,7 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  */
 fun ageDescription(age: Int): String = when {
     age % 10 == 1 && age / 10 != 1 && age / 10 != 11 -> "$age год"
-    age % 10 in 2..4 && age / 10 != 1 && age /10 != 11 -> "$age года"
+    age % 10 in 2..4 && age / 10 != 1 && age / 10 != 11 -> "$age года"
    else ->  "$age лет"
 }
 

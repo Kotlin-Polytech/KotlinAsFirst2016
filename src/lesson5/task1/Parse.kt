93add36c2dfbaecac53d7ddcd8178fbee6d8e6a4
Xepishons <Crysis10ivan@rambler.ru>
2016-12-19 01:31:05 +0300

    Lesson5 fix

--- src/lesson5/task1/Parse.kt
+++ src/lesson5/task1/Parse.kt
@@ -84,7 +84,7 @@ fun dateStrToDigit(str: String): String {
         return ""
     }
     val year = a[2].toInt()
-    return String.format("%02d.%02d.%04d", day, month, year)
+    return String.format("%02d.%02d.%d", day, month, year)
 }
 
 /**
@@ -121,7 +121,7 @@ fun dateDigitToStr(digital: String): String {
         return ""
     }
     val year = a[2].toInt()
-    return String.format("%d %s %04d", day, month, year)
+    return String.format("%d %s %d", day, month, year)
 }
 
 /**

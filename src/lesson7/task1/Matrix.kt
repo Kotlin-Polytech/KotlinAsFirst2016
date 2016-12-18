7f342ab8833637de50471fe562324d1e5df923f1
Kate <ekat.pronina@gmail.com>
2016-12-19 01:53:22 +0300

    L 7, t1, t2

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -38,32 +38,57 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
+    if ((height <= 0) || (width <= 0)) throw IllegalArgumentException()
+    val matrix = MatrixImpl(height, width, e)
+    for (row in 0..matrix.height - 1)
+        for (column in 0..matrix.width - 1)
+            matrix[row, column] = e
+    return matrix
+}
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : Matrix<E> {
+    private val list = mutableListOf<E>()
 
-    override val width: Int = TODO()
+    init {
+        for (i in 0..height * width - 1) {
+            list.add(e)
+        }
+    }
 
-    override fun get(row: Int, column: Int): E  = TODO()
+    override fun get(row: Int, column: Int): E {
+        return list[column + width * row]
+    }
 
-    override fun get(cell: Cell): E  = TODO()
+    override fun get(cell: Cell): E = get(cell.row, cell.column)
+
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        list[column + width * row] = value
+
     }
 
     override fun set(cell: Cell, value: E) {
-        TODO()
+        set(cell.row, cell.column, value)
+
     }
 
-    override fun equals(other: Any?) = TODO()
+    override fun hashCode(): Int {
+        var result = height
+        result = 31 * result + width
+        result = 31 * result + list.hashCode()
+        return result
+    }
+
+    override fun equals(other: Any?) = other is MatrixImpl<*> && height == other.height && width == other.width
 
     override fun toString(): String = TODO()
+
+
 }
 

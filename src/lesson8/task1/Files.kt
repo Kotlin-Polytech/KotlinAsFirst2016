d85f8429eeb1b096e7172583524a4a3aa92cdfe2
Pashnik <Fktrcfy12>
2016-12-20 20:19:53 +0300

    Lesson 8 task 1 And Impossible from Lesson 6 task 1

--- src/lesson8/task1/Files.kt
+++ src/lesson8/task1/Files.kt
@@ -78,7 +78,6 @@ fun sibilants(inputName: String, outputName: String) {
     for (line in File(inputName).readLines()) {
         if (line.length <= 1) {
             writer.write(line)
-            writer.newLine()
         } else {
             writer.write(line[0].toString())
         }

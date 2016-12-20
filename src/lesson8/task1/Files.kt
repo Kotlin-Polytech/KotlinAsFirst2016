a3846dbde0a751ffd480526a4356f7163f60c4d2
Pashnik <Fktrcfy12>
2016-12-20 19:41:09 +0300

    Lesson 8 task 1 And Impossible from Lesson 6 task 1

--- src/lesson8/task1/Files.kt
+++ src/lesson8/task1/Files.kt
@@ -74,24 +74,20 @@ fun countSubstrings(inputName: String, substrings: List<String>): Map<String, In
 
 fun sibilants(inputName: String, outputName: String) {
     val writer = File(outputName).bufferedWriter()
-    var sumOfLines = 0
-    var secSum = 0
-    File(inputName).bufferedReader().use {
-        for (line in it.readLines()){
-            secSum++
-        }
-    }
     val map = mapOf<String, String>("Ы" to "И", "ы" to "и", "Я" to "А", "я" to "а", "Ю" to "У", "ю" to "у")
     for (line in File(inputName).readLines()) {
-        sumOfLines++
-        for (i in 0..line.length - 1) {
+        if (line.length <= 1) {
+            writer.write(line)
+            writer.newLine()
+        } else {
+            writer.write(line[0].toString())
+        }
+        for (i in 1..line.length - 1) {
             if ((line[i] in "ЫыЯяЮю") && (line[i - 1] in "ЖжЧчШшЩщ"))
                 writer.write(map[line[i].toString()])
             else writer.write(line[i].toString())
         }
-        if (sumOfLines < secSum) {
-            writer.write("\n")
-        }
+        writer.newLine()
     }
     writer.close()
 }

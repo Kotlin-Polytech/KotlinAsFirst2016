61ef282690138e4c9bbb33f426343d576f87c2ca
El.orabi <medhelorabi@gmail.com>
2017-01-16 11:06:42 +0100

    lesson 6

--- src/lesson6/task1/Geometry.kt
+++ src/lesson6/task1/Geometry.kt
@@ -136,21 +136,27 @@ data class Line(val point: Point, val angle: Double) {
  *
  * Построить прямую по отрезку
  */
-fun lineBySegment(s: Segment): Line = TODO()
+fun lineBySegment(s: Segment): Line = Line(s.begin, Math.atan((s.end.y - s.begin.y) / (s.end.x - s.begin.x)))
 
 /**
  * Средняя
  *
  * Построить прямую по двум точкам
  */
-fun lineByPoints(a: Point, b: Point): Line = TODO()
+fun lineByPoints(a: Point, b: Point): Line = lineBySegment(Segment(a, b))
 
 /**
  * Сложная
  *
  * Построить серединный перпендикуляр по отрезку или по двум точкам
  */
-fun bisectorByPoints(a: Point, b: Point): Line = TODO()
+fun bisectorByPoints(a: Point, b: Point): Line = Line(
+        Point(
+                (a.x + b.x) / 2.0,
+                (a.y + b.y) / 2.0
+        ),
+        lineByPoints(a, b).angle + Math.PI / 2
+)
 
 /**
  * Средняя
